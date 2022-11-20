c9f13bbb73ec53190ea87d406633e85b90128dd5
anna_joachimska <ajoachimska@bee2code.com>
2022-11-20 12:45:45 +0100

    added endpoint - uploadfile

--- code/zadanie.py
+++ code/zadanie.py
@@ -1,16 +1,23 @@
-from fastapi import FastAPI, File, UploadFile, Depends
+from fastapi import FastAPI, File, UploadFile, Depends, Request
 from fastapi.security import OAuth2PasswordRequestForm
-
+from fastapi.responses import HTMLResponse
 from code.primes.primes_functions import is_prime as is_prime_function
 from code.picture.picture_functions import invert as invert_function
 from fastapi.responses import StreamingResponse
 from code.authentification.authentification_functions import login_for_access_token, oauth2_scheme, get_current_time
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="code/templates")
 
 @app.get("/prime/{number}")
 def is_prime(number: int):
    return is_prime_function(number)
+
+@app.get("/uploadpicture",  response_class=HTMLResponse)
+def uploadpic(request: Request):
+    return templates.TemplateResponse("picture_page.html",
+                                      {"request": request})
 
 @app.post("/picture")
 async def invert(file : UploadFile = File(...)):
